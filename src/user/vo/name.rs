6bd357df44f80e3efe811d0dda7c1e3749c4b138
hana <root@localhost.localdomain>
2021-02-23 14:24:44 +0900

    バリデーション追加

--- src/user/vo/name.rs
+++ src/user/vo/name.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Name {
     value: String,
@@ -5,8 +7,13 @@ pub struct Name {
 
 impl Name {
     pub fn new(value: &str) -> Self {
-        Self {
-            value: value.to_string(),
+        let regex = Regex::new(r#"^[0-9a-zA-Z]+$"#).unwrap();
+        if regex.is_match(value) {
+            Self {
+                value: value.to_string(),
+            }
+        } else {
+            panic!("invalid format")
         }
     }
 }
@@ -19,12 +26,25 @@ mod tests {
         Name::new("name1")
     }
 
+    pub fn name2() -> Name {
+        Name::new("name2")
+    }
+
     #[test]
     fn name_equals() {
+        //値によって同一と見なされること
         let name1 = self::name1();
         let name2 = self::name1();
-
-        //値による同一性が得られること
         assert_eq!(name1, name2);
+
+        //値によって同一と見なされないこと
+        let name3 = self::name2();
+        assert_ne!(name1, name3);
+    }
+
+    #[test]
+    #[should_panic(expected = "invalid format")]
+    fn name_validation() {
+        Name::new("Invalid@@@Name");
     }
 }
